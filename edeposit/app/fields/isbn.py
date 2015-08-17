39479778e63c1864a3bb2ef842bfc47bd68b344f
Jan Stavěl <stavel.jan@gmail.com>
2015-08-17 14:32:10 +0200

    fixed jstavel/edeposit#493 doplnil jsem podminku zobrazeni. Jen kdyz ma opravneni menit obsah, zobrazi se mu linka "zmenit".

--- edeposit/app/fields/isbn.py
+++ edeposit/app/fields/isbn.py
@@ -8,12 +8,18 @@ from z3c.form.browser import widget
 
 from .interfaces import IISBNWidget
 from z3c.form.browser.text import TextWidget
+from plone import api
 
 @zope.interface.implementer_only(IISBNWidget)
 class ISBNWidget(TextWidget):
     """Input type text widget implementation."""
 
     klass = u'isbn-widget'
+    
+    def canChangeISBN(self):
+        mtool = api.portal.get_tool('portal_membership')
+        result= mtool.checkPermission('Modify portal content', self.context)
+        return result
 
     def update(self):
         super(ISBNWidget, self).update()
